e514a2ff85f26d9b7d49b887f015544d74e30454
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-02-05 22:07:31 +1000

    Remove echoed setting

--- .Internal/WriteSettings.Helper.ps1
+++ .Internal/WriteSettings.Helper.ps1
@@ -29,9 +29,10 @@ function Set-JsonContentLF {
         Set-ContentLF -path $path -content ($object | ConvertTo-Json -Depth 99)
         if ($PSVersionTable.PSVersion.Major -lt 6) {
             try {
+                Write-Host "Formatting JSON file with LF line endings using pwsh as PowerShell 7 would do it"
                 $path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path)
                 # This command will reformat a JSON file with LF line endings as PowerShell 7 would do it (when run using pwsh)
-                $command = "`$cr=[char]13;`$lf=[char]10;`$path='$path';`$content=Get-Content `$path -Encoding UTF8|ConvertFrom-Json|ConvertTo-Json -Depth 99;`$content=`$content -replace `$cr,'';`$content|Out-Host;[System.IO.File]::WriteAllText(`$path,`$content+`$lf)"
+                $command = "`$cr=[char]13;`$lf=[char]10;`$path='$path';`$content=Get-Content `$path -Encoding UTF8|ConvertFrom-Json|ConvertTo-Json -Depth 99;`$content=`$content -replace `$cr,'';[System.IO.File]::WriteAllText(`$path,`$content+`$lf)"
                 . pwsh -command $command
             }
             catch {
