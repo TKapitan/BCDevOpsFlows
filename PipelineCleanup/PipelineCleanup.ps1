c0a0b58e6e864c06832b0b8161a35a4770e1efd2
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-02-08 23:21:34 +1000

    Add details to log about cleanup

--- PipelineCleanup/PipelineCleanup.ps1
+++ PipelineCleanup/PipelineCleanup.ps1
@@ -1,16 +1,20 @@
 # Clean Containers
 if ($ENV:AL_CONTAINERNAME) {
+    Write-Host "Cleaning container $ENV:AL_CONTAINERNAME"
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\.Internal\BCContainerHelper.Helper.ps1" -Resolve)
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\RunPipeline\RunPipeline.Helper.ps1" -Resolve)
 
     DownloadAndImportBcContainerHelper
+    Write-Host "Removing container $ENV:AL_CONTAINERNAME"
     Remove-Bccontainer GetContainerName
 }
 
 # Clean Nuget
 if ($ENV:AL_NUGETINITIALIZED) {
+    Write-Host "Cleaning Nuget packages"
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\.Internal\Nuget.Helper.ps1" -Resolve)
     
+    Write-Host "Removing Nuget packages from $ENV:PIPELINE_WORKSPACE\App\.alpackages"
     $packageCachePath = "$ENV:PIPELINE_WORKSPACE\App\.alpackages"
     Remove-Item $packageCachePath -Recurse -Include *.*
 }
