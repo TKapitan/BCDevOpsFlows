13782499d6bb5f1ee154afbf560c90973ff79c65
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-02-08 21:25:19 +1000

    Issue debug

--- InitNuget/InitNuget.Helper.ps1
+++ InitNuget/InitNuget.Helper.ps1
@@ -1,7 +1,7 @@
 . (Join-Path -Path $PSScriptRoot -ChildPath "..\.Internal\Nuget.Helper.ps1" -Resolve)
 
 function InstallAndRegisterNugetPackageProvider() {
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-    Install-PackageProvider -Name NuGet -Force
-    AddNugetPackageSource -sourceName 'nuget.org' -sourceUrl 'https://api.nuget.org/v3/index.json'
+    # [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+    # Install-PackageProvider -Name NuGet -Force
+    # AddNugetPackageSource -sourceName 'nuget.org' -sourceUrl 'https://api.nuget.org/v3/index.json'
 }
