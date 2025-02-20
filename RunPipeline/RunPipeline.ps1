0b0cbf6cc75e3b8a68ed4f860e13332356e66730
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-02-20 17:37:44 +1000

    Convert resourceexposurepolicy to array

--- RunPipeline/RunPipeline.ps1
+++ RunPipeline/RunPipeline.ps1
@@ -253,11 +253,11 @@ try {
         
         $resourceExposurePolicySpecified = [bool] ($appFileJson.PSObject.Properties.Name -eq 'resourceExposurePolicy')
         if (!$resourceExposurePolicySpecified) {
-            $resourceExposurePolicy = @()
+            $resourceExposurePolicy = @() | ConvertFrom-Json | ConvertTo-HashTable
             OutputDebug -Message "Setting 'resourceExposurePolicy' using settings from pipeline. No existing setting found in app.json"
         }
         else {
-            $resourceExposurePolicy = @($appFileJson.resourceExposurePolicy)
+            $resourceExposurePolicy = $appFileJson.resourceExposurePolicy | ConvertFrom-Json | ConvertTo-HashTable
             OutputDebug -Message "Setting 'resourceExposurePolicy' using settings from pipeline and existing app.json setting"
         }
 
