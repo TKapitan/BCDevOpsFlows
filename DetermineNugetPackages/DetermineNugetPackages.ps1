e9a4145795a7c068856487587b30ba0b9fd0b688
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-03-20 01:46:01 +1000

    Try/catch for determine nuget package

--- DetermineNugetPackages/DetermineNugetPackages.ps1
+++ DetermineNugetPackages/DetermineNugetPackages.ps1
@@ -6,28 +6,37 @@ if (!$ENV:AL_NUGETINITIALIZED) {
     Write-Error "Nuget not initialized - make sure that the InitNuget pipeline step is configured to run before this step."
 }
 
-$settings = $ENV:AL_SETTINGS | ConvertFrom-Json
+try {
+    $settings = $ENV:AL_SETTINGS | ConvertFrom-Json
 
-AddNugetPackageSource -sourceName "MSSymbols" -sourceUrl "https://dynamicssmb2.pkgs.visualstudio.com/DynamicsBCPublicFeeds/_packaging/MSSymbols/nuget/v3/index.json"
-AddNugetPackageSource -sourceName "AppSourceSymbols" -sourceUrl "https://dynamicssmb2.pkgs.visualstudio.com/DynamicsBCPublicFeeds/_packaging/AppSourceSymbols/nuget/v3/index.json"
+    AddNugetPackageSource -sourceName "MSSymbols" -sourceUrl "https://dynamicssmb2.pkgs.visualstudio.com/DynamicsBCPublicFeeds/_packaging/MSSymbols/nuget/v3/index.json"
+    AddNugetPackageSource -sourceName "AppSourceSymbols" -sourceUrl "https://dynamicssmb2.pkgs.visualstudio.com/DynamicsBCPublicFeeds/_packaging/AppSourceSymbols/nuget/v3/index.json"
 
-$baseRepoFolder = "$ENV:PIPELINE_WORKSPACE\App"
-$baseAppFolder = "$baseRepoFolder\App"
+    $baseRepoFolder = "$ENV:PIPELINE_WORKSPACE\App"
+    $baseAppFolder = "$baseRepoFolder\App"
 
-$applicationPackage = "Microsoft.Application.symbols"
-if ($settings.country) {
-    $applicationPackage = "Microsoft.Application.$($settings.country.ToUpper()).symbols"
-}
-$manifestObject = Get-Content "$baseAppFolder\app.json" -Encoding UTF8 | ConvertFrom-Json
+    $applicationPackage = "Microsoft.Application.symbols"
+    if ($settings.country) {
+        $applicationPackage = "Microsoft.Application.$($settings.country.ToUpper()).symbols"
+    }
+    $manifestObject = Get-Content "$baseAppFolder\app.json" -Encoding UTF8 | ConvertFrom-Json
  
-$packageCachePath = "$baseRepoFolder\.alpackages"
-mkdir $packageCachePath
+    $packageCachePath = "$baseRepoFolder\.alpackages"
+    mkdir $packageCachePath
  
-nuget install $applicationPackage -outputDirectory $packageCachePath 
+    nuget install $applicationPackage -outputDirectory $packageCachePath 
  
-foreach ($Dependency in $manifestObject.dependencies) {
-    $PackageName = ("{0}.{1}.symbols.{2}" -f $Dependency.publisher, $Dependency.name, $Dependency.id ) -replace ' ', ''
-    Write-Host "Get $PackageName"
+    foreach ($Dependency in $manifestObject.dependencies) {
+        $PackageName = ("{0}.{1}.symbols.{2}" -f $Dependency.publisher, $Dependency.name, $Dependency.id ) -replace ' ', ''
+        Write-Host "Get $PackageName"
          
-    nuget install $PackageName -outputDirectory $packageCachePath 
+        nuget install $PackageName -outputDirectory $packageCachePath 
+    }
+}
+catch {
+    Write-Host $_.Exception -ForegroundColor Red
+    Write-Host $_.ScriptStackTrace
+    Write-Host $_.PSMessageDetails
+
+    Write-Error "Process failed. See previous lines for details."
 }
