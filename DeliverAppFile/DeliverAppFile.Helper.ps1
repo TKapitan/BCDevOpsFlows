4ee0f70ecaa000ee10546e15dfd7e7dc404cd92b
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-03-19 22:55:06 +1000

    Remove legacy code

--- DeliverAppFile/DeliverAppFile.Helper.ps1
+++ DeliverAppFile/DeliverAppFile.Helper.ps1
@@ -33,24 +33,3 @@ function Push-AppToNuGetFeed {
     $package = New-BcNuGetPackage @parameters
     Push-BcNuGetPackage -nuGetServerUrl $url -nuGetToken $token -bcNuGetPackage $package
 }
-
-function Get-AppTargetFilePathForNewApp {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory)]
-        [string] $writableFolderPath,
-        [Parameter(Mandatory)]
-        $appFileJson,
-        [switch] $isPreview
-    )
-
-    $releaseTypeFolderParam = @{}
-    if ($isPreview -eq $true) {
-        $releaseTypeFolderParam = @{ "isPreview" = $true }
-    }
-    
-    $releaseTypeFolder = Get-ReleaseTypeFolderName @releaseTypeFolderParam
-    $targetFilePath = "$writableFolderPath\apps\$releaseTypeFolder\$($appFileJson.id)\$($appFileJson.version)-BC$($appFileJson.application)\"
-    Write-Host "Using '$targetFilePath' regardless if the extension exists or not"
-    return $targetFilePath
-}
