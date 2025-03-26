9de555d663cc1cf703578364b2b0e78ff02332ae
Tomas Kapitan <kapitan@kepty.cz>
2025-03-26 23:09:45 +1000

    Update Get-BCDevOpsFlowsNuGetPackage to reflect latest changes (#83)

--- .Internal/NuGet/Get-BCDevOpsFlowsNuGetPackage.ps1
+++ .Internal/NuGet/Get-BCDevOpsFlowsNuGetPackage.ps1
@@ -9,12 +9,8 @@
   string
   Path to the a tmp folder where the package is downloaded
   This folder should be deleted after usage
- .PARAMETER nuGetServerUrl
-  NuGet Server URL
-  Default: https://api.nuget.org/v3/index.json
- .PARAMETER nuGetToken
-  NuGet Token for authenticated access to the NuGet Server
-  If not specified, the NuGet Server is accessed anonymously (and needs to support this)
+ .PARAMETER trustedNugetFeeds
+  Array of objects with nuget feeds to trust in the format @([PSCustomObject]@{Url="https://api.nuget.org/v3/index.json"; Token=""; Patterns=@('*'); Fingerprints=@()})
  .PARAMETER packageName
   Package Name to search for.
   This can be the full name or a partial name with wildcards.
@@ -35,15 +31,13 @@
  .EXAMPLE
   Get-BCDevOpsFlowsNuGetPackage -packageName 'FreddyKristiansen.BingMapsPTE.165d73c1-39a4-4fb6-85a5-925edc1684fb'
  .EXAMPLE
-  Get-BCDevOpsFlowsNuGetPackage -nuGetServerUrl $nugetServerUrl -nuGetToken $nuGetToken -packageName '437dbf0e-84ff-417a-965d-ed2bb9650972' -allowPrerelease
+  Get-BCDevOpsFlowsNuGetPackage -trustedNugetFeeds $trustedNugetFeeds -packageName '437dbf0e-84ff-417a-965d-ed2bb9650972' -allowPrerelease
 #>
 Function Get-BCDevOpsFlowsNuGetPackage {
     Param(
         [Parameter(Mandatory = $false)]
-        [string] $nuGetServerUrl = "",
+        $trustedNugetFeeds = @([PSCustomObject]@{"Url" = "https://api.nuget.org/v3/index.json"; "Token" = ""; "Patterns" = @('*'); "Fingerprints" = @() }),
         [Parameter(Mandatory = $false)]
-        [string] $nuGetToken = "",
-        [Parameter(Mandatory = $true)]
         [string] $packageName,
         [Parameter(Mandatory = $false)]
         [string] $version = '0.0.0.0',
@@ -56,7 +50,7 @@ Function Get-BCDevOpsFlowsNuGetPackage {
     )
 
     try {    
-        $feed, $packageId, $packageVersion = Find-BCDevOpsFlowsNugetPackage -nuGetServerUrl $nuGetServerUrl -nuGetToken $nuGetToken -packageName $packageName -version $version -excludeVersions $excludeVersions -verbose:($VerbosePreference -eq 'Continue') -select $select -allowPrerelease:($allowPrerelease.IsPresent)
+        $feed, $packageId, $packageVersion = Find-BCDevOpsFlowsNugetPackage -trustedNugetFeeds $trustedNugetFeeds -packageName $packageName -version $version -excludeVersions $excludeVersions -verbose:($VerbosePreference -eq 'Continue') -select $select -allowPrerelease:($allowPrerelease.IsPresent)
         if (-not $feed) {
             Write-Host "No package found matching package name $($packageName) Version $($version)"
             return ''
