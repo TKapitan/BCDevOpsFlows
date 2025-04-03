8b4a997139f1af2e6fad0aa13e8b678799f08eeb
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-04-03 19:41:22 +1000

    Add missing quotes

--- SetupPipelines/SetupPipelines.ps1
+++ SetupPipelines/SetupPipelines.ps1
@@ -7,7 +7,7 @@ az extension add -n azure-devops
 az --version
 
 # Set the default Azure DevOps organization and project
-az devops configure --defaults organization=$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI project= $ENV:SYSTEM_TEAMPROJECT --use-git-aliases true
+az devops configure --defaults organization="$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" project="$ENV:SYSTEM_TEAMPROJECT" --use-git-aliases true
 
 # Show build list and PRs
 az pipelines build list
