3da20cd31edda0aa2e80da3899a13c04b5f9dedc
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-04-03 19:56:04 +1000

    specify --repository in az pipelines create

--- SetupPipelines/SetupPipelines.ps1
+++ SetupPipelines/SetupPipelines.ps1
@@ -1,18 +1,11 @@
 Param()
 
-# Install Azure DevOps extension
 az extension add -n azure-devops
-
-# Set the default Azure DevOps organization and project
 az devops configure --defaults organization="$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" project="$ENV:SYSTEM_TEAMPROJECT" --use-git-aliases true
-
-# Show build list and PRs
-az pipelines build list
-git pr list
 
 $yamlFolder = "$ENV:BUILD_REPOSITORY_LOCALPATH\.azure-pipelines\Templates"
 $yamlFiles = Get-ChildItem -Path $yamlFolder -Filter *.yml -File
 foreach ($pipelineYamlFile in $yamlFiles) {
     $pipelineName = $pipelineYamlFile.BaseName
-    az pipelines create --name $pipelineName --description 'Test' --branch test --yml-path $pipelineName
+    az pipelines create --name $pipelineName --description 'Test' --repository $ENV:BUILD_REPOSITORY_NAME --branch test --yml-path $pipelineName
 }
