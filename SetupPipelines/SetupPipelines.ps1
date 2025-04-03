6eb932b797c00d7e64fc9a5416994ae27021f284
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-04-03 19:59:06 +1000

    Print function details

--- SetupPipelines/SetupPipelines.ps1
+++ SetupPipelines/SetupPipelines.ps1
@@ -1,5 +1,7 @@
 Param()
 
+. (Join-Path -Path $PSScriptRoot -ChildPath "WriteOutput.Helper.ps1" -Resolve)
+
 az extension add -n azure-devops
 az devops configure --defaults organization="$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" project="$ENV:SYSTEM_TEAMPROJECT" --use-git-aliases true
 
@@ -7,5 +9,7 @@ $yamlFolder = "$ENV:BUILD_REPOSITORY_LOCALPATH\.azure-pipelines\Templates"
 $yamlFiles = Get-ChildItem -Path $yamlFolder -Filter *.yml -File
 foreach ($pipelineYamlFile in $yamlFiles) {
     $pipelineName = $pipelineYamlFile.BaseName
+    
+    OutputDebug "az pipelines create --name $pipelineName --description 'Test' --repository $ENV:BUILD_REPOSITORY_NAME --branch test --yml-path $pipelineName"
     az pipelines create --name $pipelineName --description 'Test' --repository $ENV:BUILD_REPOSITORY_NAME --branch test --yml-path $pipelineName
 }
