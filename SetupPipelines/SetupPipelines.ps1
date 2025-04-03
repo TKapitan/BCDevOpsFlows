bab56805cb7e680490ae1a1ab0ed8c4ee4b516de
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-04-03 19:53:53 +1000

    create pipelines from yaml files

--- SetupPipelines/SetupPipelines.ps1
+++ SetupPipelines/SetupPipelines.ps1
@@ -3,12 +3,16 @@ Param()
 # Install Azure DevOps extension
 az extension add -n azure-devops
 
-# Show Azure CLI version
-az --version
-
 # Set the default Azure DevOps organization and project
 az devops configure --defaults organization="$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" project="$ENV:SYSTEM_TEAMPROJECT" --use-git-aliases true
 
 # Show build list and PRs
 az pipelines build list
 git pr list
+
+$yamlFolder = "$ENV:BUILD_REPOSITORY_LOCALPATH\.azure-pipelines\Templates"
+$yamlFiles = Get-ChildItem -Path $yamlFolder -Filter *.yml -File
+foreach ($pipelineYamlFile in $yamlFiles) {
+    $pipelineName = $pipelineYamlFile.BaseName
+    az pipelines create --name $pipelineName --description 'Test' --branch test --yml-path $pipelineName
+}
