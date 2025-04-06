1a9f409eaa1496e24bbf574315423fea22214fa4
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-04-06 21:31:02 +1000

    Remove imports

--- WorkflowInitialize/WorkflowInitialize.ps1
+++ WorkflowInitialize/WorkflowInitialize.ps1
@@ -8,6 +8,3 @@ $scopeJson = @{
 $env:AL_TELEMETRYSCOPE = $scopeJson
 Write-Host "##vso[task.setvariable variable=AL_TELEMETRYSCOPE;]$scopeJson"
 OutputDebug -Message "Set environment variable AL_TELEMETRYSCOPE to ($env:AL_TELEMETRYSCOPE)"
-
-Install-Module -Name ConvertTo-Hashtable -Force -Verbose -Scope CurrentUser
-Import-Module ConvertTo-Hashtable
