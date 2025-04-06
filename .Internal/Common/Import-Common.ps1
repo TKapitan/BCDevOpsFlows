ab38aa86e832db6bd8abdafcbe877af549009805
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-04-06 21:41:42 +1000

    CmdDo

--- .Internal/Common/Import-Common.ps1
+++ .Internal/Common/Import-Common.ps1
@@ -1,3 +1,5 @@
+. (Join-Path $PSScriptRoot "CmdDo.ps1")
+
 . (Join-Path $PSScriptRoot "ConvertTo-HashTable.ps1")
 . (Join-Path $PSScriptRoot "ConvertTo-OrderedDictionary.ps1")
 . (Join-Path $PSScriptRoot "Invoke-git.ps1")
