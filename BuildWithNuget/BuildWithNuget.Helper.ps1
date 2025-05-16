dc6abb26b059b9a3b37f4722d854418f69a53530
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-05-16 21:50:33 +1000

    Build Details + Preprocessor symbols support in build with nuget

--- BuildWithNuget/BuildWithNuget.Helper.ps1
+++ BuildWithNuget/BuildWithNuget.Helper.ps1
@@ -6,6 +6,7 @@ function Assert-Prerequisites {
 
 function Get-BuildParameters {
     param (
+        $settings,
         [string]$baseRepoFolder,
         [string]$baseAppFolder,
         [string]$packageCachePath,
@@ -15,22 +16,40 @@ function Get-BuildParameters {
     $AppFileName = (("{0}_{1}_{2}.app" -f $appFileJson.publisher, $appFileJson.name, $appFileJson.version).Split([System.IO.Path]::GetInvalidFileNameChars()) -join '')
     $outputPath = Join-Path -Path $ENV:BUILD_REPOSITORY_LOCALPATH -ChildPath ".output"
 
-    return @(
-        "/project:`"$baseAppFolder`"",
-        "/packagecachepath:$packageCachePath",
-        "/out:`"$outputPath\$AppFileName`"",
+    $alcItem = Get-Item -Path (Join-Path $ENV:AL_BCDEVTOOLSFOLDER 'alc.exe')
+    [System.Version]$alcVersion = $alcItem.VersionInfo.FileVersion
+    $alcParameters = @(
+        "/project:""$baseAppFolder""", 
+        "/packagecachepath:""$packageCachePath""", 
+        "/out:""$outputPath\$AppFileName""",
         "/loglevel:Warning"
     )
+    if ($alcVersion -ge [System.Version]"12.0.12.41479") {
+        $alcParameters = @(
+            "sourceRepositoryUrl:""$ENV:BUILD_REPOSITORY_URI""",
+            "sourceCommit:""$ENV:BUILD_SOURCEVERSION""",
+            "buildBy:""BCDevOpsFlows""",
+            "buildUrl:""$ENV:BUILD_BUILDURI"""
+        )
+    }
+    if ($settings.ContainsKey('preprocessorSymbols')) {
+        Write-Host "Adding Preprocessor symbols : $($settings.preprocessorSymbols -join ',')"
+        $settings.preprocessorSymbols | where-Object { $_ } | ForEach-Object { $alcParameters += @("/D:$_") }
+    }
+    return $alcParameters
 }
 
 function Invoke-AlCompiler {
-    param([array]$Parameters)
+    param(
+        [array]$Parameters
+    )
 
     Write-Host "Using parameters:"
     $Parameters | ForEach-Object { Write-Host "  $_" }
 
     Push-Location
     try {
+        Write-Host ".\alc.exe $([string]::Join(' ', $Parameters))"
         Set-Location $ENV:AL_BCDEVTOOLSFOLDER
         & .\alc.exe $Parameters
     }
