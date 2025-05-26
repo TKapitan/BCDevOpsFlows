1134e65d77eb0feb942e51c246fd54cdfbd74d3a
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-05-26 14:16:03 +0200

    Try/catch in build step

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -11,15 +11,26 @@ if ([string]::IsNullOrEmpty($ENV:AL_RUNWITH)) {
 Write-Host "Identifying what engine to use for build: " $ENV:AL_RUNWITH
 $runWith = ($ENV:AL_RUNWITH).ToLowerInvariant()
 
-$buildParameters = @{
-    "artifact"  = $artifact
-    "buildMode" = $buildMode
+try {
+    $buildParameters = @{
+        "artifact"  = $artifact
+        "buildMode" = $buildMode
+    }
+    if ($runWith -eq 'nuget') {
+        Write-Host "Using NuGet"
+        . (Join-Path -Path $PSScriptRoot -ChildPath "WithNuGet\BuildWithNuget.ps1" -Resolve) @buildParameters
+    }
+    elseif ($runWith -eq 'bccontainerhelper') {
+        Write-Host "Using BCContainerHelper"
+        . (Join-Path -Path $PSScriptRoot -ChildPath "WithBCContainerHelper\BuildWithBCContainerHelper.ps1" -Resolve) @buildParameters
+    }
 }
-if ($runWith -eq 'nuget') {
-    Write-Host "Using NuGet"
-    . (Join-Path -Path $PSScriptRoot -ChildPath "WithNuGet\BuildWithNuget.ps1" -Resolve) @buildParameters
-}
-elseif ($runWith -eq 'bccontainerhelper') {
-    Write-Host "Using BCContainerHelper"
-    . (Join-Path -Path $PSScriptRoot -ChildPath "WithBCContainerHelper\BuildWithBCContainerHelper.ps1" -Resolve) @buildParameters
+catch {
+    Write-Host "##vso[task.logissue type=error]Error while running the build using $($ENV:AL_RUNWITH). Error message: $($_.Exception.Message)"
+    Write-Host $_.ScriptStackTrace
+    if ($_.PSMessageDetails) {
+        Write-Host $_.PSMessageDetails
+    }
+    Write-Host "##vso[task.complete result=Failed]"
+    exit 0
 }
