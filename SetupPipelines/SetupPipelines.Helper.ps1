9ad0794a58ab03e7e65479272f87debd04996336
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-06-03 16:39:25 +0200

    delete pipeline if exist when running setuppipelines

--- SetupPipelines/SetupPipelines.Helper.ps1
+++ SetupPipelines/SetupPipelines.Helper.ps1
@@ -64,7 +64,6 @@ function Add-AzureDevOpsPipelineFromYaml {
 
     $existingPipelineDetails = az pipelines list `
         --name "$pipelineName" `
-        --folder-path "$pipelineFolder" `
         --organization "$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" `
         --project "$ENV:SYSTEM_TEAMPROJECT" `
         --repository "$ENV:BUILD_REPOSITORY_NAME" `
@@ -72,17 +71,14 @@ function Add-AzureDevOpsPipelineFromYaml {
         
     OutputDebug "Existing pipeline details: $existingPipelineDetails"
     if ($existingPipelineDetails.Count -gt 0) {
-        Write-Host "Pipeline $pipelineName in folder $pipelineFolder already exists. Updating existing..."
-        az pipelines update `
-            --id $existingPipelineDetails[0].id `
-            --new-name "$pipelineName" `
-            --new-folder-path "$pipelineFolder" `
-            --organization "$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" `
-            --project "$ENV:SYSTEM_TEAMPROJECT" `
-            --description "Pipeline $pipelineName created by SetupPipelines." `
-            --branch $pipelineBranch `
-            --yml-path "$pipelineYamlFileRelativePath"
-        return
+        Write-Host "Pipeline $pipelineName exists. Removing..."
+        foreach ($pipeline in $existingPipelineDetails) {
+            az pipelines delete `
+                --id $pipeline.id `
+                --organization "$ENV:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" `
+                --project "$ENV:SYSTEM_TEAMPROJECT" `
+                --yes
+        }
     }
 
     Write-Host "Creating pipeline $pipelineName in folder $pipelineFolder"
