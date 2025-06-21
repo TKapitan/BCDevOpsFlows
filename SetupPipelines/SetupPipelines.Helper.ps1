f5576a2883f80e8b51906cc6d09bf84fc19cc626
Tomas Kapitan <tomas.kapitan@fusion5.com.au>
2025-06-21 13:56:24 +1000

    service connection ID

--- SetupPipelines/SetupPipelines.Helper.ps1
+++ SetupPipelines/SetupPipelines.Helper.ps1
@@ -98,6 +98,7 @@ function Add-AzureDevOpsPipelineFromYaml {
         --branch $pipelineBranch `
         --yml-path "$pipelineYamlFileRelativePath" `
         --repository-type "$ENV:BUILD_REPOSITORY_PROVIDER" `
+        --service-connection "ea610565-0884-439a-8907-ad5b7ce1e3df" `
         --skip-first-run $skipPipelineFirstRun
 }
 
