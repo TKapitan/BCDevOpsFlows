1acef2b047711e8f00230a79ecd9e700a463f351
Tom Kapitan <tomas.kapitan@fusion5.com.au>
2025-09-20 21:35:59 +0200

    Empty custom logic

--- CustomLogic/GetDependencyVersionFilter.ps1
+++ CustomLogic/GetDependencyVersionFilter.ps1
@@ -5,16 +5,5 @@ function GetDependencyVersionFilter {
         [Parameter(Mandatory = $true)]
         [PSCustomObject] $dependency
     )
-    if ($dependency.publisher.Replace(' ', '') -eq 'Microsoft') {
-        Write-Host "Dependency $($dependency.name) is from Microsoft, no version filter applied."
-        return ""
-    }
-    if ($appJson.publisher.Replace(' ', '') -ne $dependency.publisher.Replace(' ', '')) {
-        Write-Host "Dependency $($dependency.name) is from different publisher ($($dependency.publisher)) than the main app ($($appJson.publisher)), using exact version match."
-        return "[$($dependency.version)]"
-    }
-    $versionParts = $appJson.application.Split('.')
-    $versionParts[1] = ([int]$versionParts[1] + 1).ToString().PadLeft(2, '0')
-    Write-Host "Dependency $($dependency.name) is from the same publisher ($($dependency.publisher)) as the main app ($($appJson.publisher)), using version range [$($dependency.version),$($versionParts[0])$($versionParts[1]).0.0.0)."
-    return "[$($dependency.version),$($versionParts[0])$($versionParts[1]).0.0.0)"
+    return ""
 }
