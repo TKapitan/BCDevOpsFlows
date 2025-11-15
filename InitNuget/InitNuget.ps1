409e8d1d1c7635856bb8bdc24c4904e72c273752
Tom Kapitan <tomas.kapitan@fusion5.com.au>
2025-11-15 17:01:33 +1000

    Download nuget BC tools only when building with nuget

--- InitNuget/InitNuget.ps1
+++ InitNuget/InitNuget.ps1
@@ -6,41 +6,42 @@ Param()
 . (Join-Path -Path $PSScriptRoot -ChildPath "..\.Internal\WriteOutput.Helper.ps1" -Resolve)
 
 try {
-    $settings = $ENV:AL_SETTINGS | ConvertFrom-Json | ConvertTo-HashTable
-    if (!$settings) {
-        throw "Settings not found - make sure that the ReadSettings pipeline step is configured to run before this step."
-    }
+    if ($ENV:AL_RUNWITH -eq "NuGet") {
+        $settings = $ENV:AL_SETTINGS | ConvertFrom-Json | ConvertTo-HashTable
+        if (!$settings) {
+            throw "Settings not found - make sure that the ReadSettings pipeline step is configured to run before this step."
+        }
 
-    $bcDevToolsPackageName = "Microsoft.Dynamics.BusinessCentral.Development.Tools"
-    $params = @{ 
-        "Source" = "https://api.nuget.org/v3/index.json"
-    }
-    $majorVersion = [Version]::Parse($(Get-AppJson -settings $settings).application).Major
-    $compilerVersion = $majorVersion - 11
-    if ($compilerVersion -le 16) {
-        $compilerVersion = 16 # v15 and earlier NuGets are not available or not correct
-    }
-    $params += @{ 
-        "MinimumVersion" = "$compilerVersion.0.0.0"
-        "MaximumVersion" = "$($compilerVersion + 1).0.0.0"
-    }
-    OutputDebug -Message "Find-Package results:"
-    $searchResultsAll = Find-Package $bcDevToolsPackageName -AllowPrereleaseVersions -AllVersions @params
-    $searchResultsAll | ForEach-Object {
-        OutputDebug -Message "Name: $($_.Name), Version: $($_.Version)"
-    }
-    $bcDevToolsPackageVersion = $($searchResultsAll | Sort-Object Version -Descending | Select-Object -First 1).Version
-    if ([string]::IsNullOrEmpty($bcDevToolsPackageVersion)) {
-        throw "Could not determine BC Dev Tools version from NuGet search results"
-    }
-    Write-Host "Using $bcDevToolsPackageName version $bcDevToolsPackageVersion"
-    DownloadNugetPackage -packageName $bcDevToolsPackageName -packageVersion $bcDevToolsPackageVersion
+        $bcDevToolsPackageName = "Microsoft.Dynamics.BusinessCentral.Development.Tools"
+        $params = @{ 
+            "Source" = "https://api.nuget.org/v3/index.json"
+        }
+        $majorVersion = [Version]::Parse($(Get-AppJson -settings $settings).application).Major
+        $compilerVersion = $majorVersion - 11
+        if ($compilerVersion -le 16) {
+            $compilerVersion = 16 # v15 and earlier NuGets are not available or not correct
+        }
+        $params += @{ 
+            "MinimumVersion" = "$compilerVersion.0.0.0"
+            "MaximumVersion" = "$($compilerVersion + 1).0.0.0"
+        }
+        OutputDebug -Message "Find-Package results:"
+        $searchResultsAll = Find-Package $bcDevToolsPackageName -AllowPrereleaseVersions -AllVersions @params
+        $searchResultsAll | ForEach-Object {
+            OutputDebug -Message "Name: $($_.Name), Version: $($_.Version)"
+        }
+        $bcDevToolsPackageVersion = $($searchResultsAll | Sort-Object Version -Descending | Select-Object -First 1).Version
+        if ([string]::IsNullOrEmpty($bcDevToolsPackageVersion)) {
+            throw "Could not determine BC Dev Tools version from NuGet search results"
+        }
+        Write-Host "Using $bcDevToolsPackageName version $bcDevToolsPackageVersion"
+        DownloadNugetPackage -packageName $bcDevToolsPackageName -packageVersion $bcDevToolsPackageVersion
 
-    $bcDevToolsFolder = Join-Path -Path (GetNugetPackagePath -packageName $bcDevToolsPackageName -packageVersion $bcDevToolsPackageVersion) -ChildPath "Tools\net8.0\any"
-    $ENV:AL_BCDEVTOOLSFOLDER = $bcDevToolsFolder
-    Write-Host "##vso[task.setvariable variable=AL_BCDEVTOOLSFOLDER;]$bcDevToolsFolder"
-    OutputDebug -Message "Set environment variable AL_BCDEVTOOLSFOLDER to ($ENV:AL_BCDEVTOOLSFOLDER)"
-
+        $bcDevToolsFolder = Join-Path -Path (GetNugetPackagePath -packageName $bcDevToolsPackageName -packageVersion $bcDevToolsPackageVersion) -ChildPath "Tools\net8.0\any"
+        $ENV:AL_BCDEVTOOLSFOLDER = $bcDevToolsFolder
+        Write-Host "##vso[task.setvariable variable=AL_BCDEVTOOLSFOLDER;]$bcDevToolsFolder"
+        OutputDebug -Message "Set environment variable AL_BCDEVTOOLSFOLDER to ($ENV:AL_BCDEVTOOLSFOLDER)"
+    }
     $ENV:AL_NUGETINITIALIZED = $true
     Write-Host "##vso[task.setvariable variable=AL_NUGETINITIALIZED;]$true"
     OutputDebug -Message "Set environment variable AL_NUGETINITIALIZED to ($ENV:AL_NUGETINITIALIZED)"
